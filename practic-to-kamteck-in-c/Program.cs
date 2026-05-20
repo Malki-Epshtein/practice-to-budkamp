358b067ec794c7b481604a67a246985445c82065
Malki-Epshtein <malki40171@gmail.com>
2026-05-20 19:19:12 +0300

    READNAME AND MAIN

---
+++ practic-to-kamteck-in-c/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace practic_to_kamteck_in_c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
